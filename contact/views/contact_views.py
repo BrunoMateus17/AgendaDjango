34d09aa7a26e7aaaa07c2f2496cd32353bf6b12b
Bruno <brunomateus2017@hotmail.com>
2024-05-17 16:33:51 -0300

    Atualização

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
-from django.http import Http404
+from django.contrib.auth.models import User
+
 from django.core.paginator import Paginator
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')
+    print(request.user)
+    contacts = Contact.objects.filter(show=True,owner_id=request.user.id).order_by('-id')
+    # contacts = Contact.objects.filter(show=True,owner_id=request.user.id).order_by('-id')
     paginator = Paginator(contacts, 20)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -29,8 +32,6 @@ def search(request):
     }
    
     return render(request,'contact/index.html',context)
-
-
 
 def contact(request,contact_id):
     single_contact = get_object_or_404(Contact,pk=contact_id,show=True)
